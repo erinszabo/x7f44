706013d8844799e3423e09622c3f83477f98ba42
Erin Szabo <eszabo@cocc.edu>
2021-12-08 11:52:37 -0800

    remove getTiles and make tiles readable

--- box.rb
+++ box.rb
@@ -1,6 +1,6 @@
 class Box
 
-  attr_reader :shut_tiles, :number_of_tiles
+  attr_reader :shut_tiles, :number_of_tiles, :tiles
 
   def initialize(number_of_tiles)
     @number_of_tiles = number_of_tiles
@@ -21,8 +21,8 @@ class Box
     false
   end
 
-  def getTiles
-    @tiles
-  end
+  # def getTiles
+  #   @tiles
+  # end
 
 end
